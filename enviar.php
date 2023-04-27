efc22da95b0d8745ade3e4a79f4da94257b794d1
Jailer Narvaez <jailernarvaez1999@gmail.com>
2023-04-26 20:54:14 -0500

    Pasando el metodo del formulario a get para que no de error 405

--- enviar.php
+++ enviar.php
@@ -1,15 +1,15 @@
-if (isset($_REQUEST['nombre']) && isset($_REQUEST['email']) && isset($_REQUEST['mensaje'])) {
-	$name = $_REQUEST['nombre'];
-	$email = $_REQUEST['email'];
-	$message = $_REQUEST['mensaje'];
-	$to = "jailernarvaez1999@gmail.com";
-	$subject = "Formulario de contacto";
-	$body = "Nombre: $name\nEmail: $email\nMensaje: $message";
-	$headers = "From: $email";
+if (isset($_GET['nombre']) && isset($_GET['email']) && isset($_GET['mensaje'])) {
+    $name = $_GET['nombre'];
+    $email = $_GET['email'];
+    $message = $_GET['mensaje'];
+    $to = "jailernarvaez1999@gmail.com";
+    $subject = "Formulario de contacto";
+    $body = "Nombre: $name\nEmail: $email\nMensaje: $message";
+    $headers = "From: $email";
 
-	if (mail($to, $subject, $body, $headers)) {
-		echo "Mensaje enviado correctamente.";
-	} else {
-		echo "Error al enviar el mensaje.";
-	}
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensaje enviado correctamente.";
+    } else {
+        echo "Error al enviar el mensaje.";
+    }
 }
